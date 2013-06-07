dbe4d3232db44fb5f3fc313175bea54498e65bdc
Dave Marchevsky <dm@bit.ly>
2013-06-07 13:52:46 -0400

    Cities.csv

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request
 from flask import jsonify
-import requests
+import requests, json
+
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,8 +10,14 @@ def home():
 
 @app.route("/city_latlon")
 def get_latlon_from_city():
-    city = request.args.get('city')
-    return city 
+    try:
+        city = request.args.get('city')
+        req = requests.get('http://nominatim.openstreetmap.org?format=json&city='+city)
+        j = json.loads(req.text)
+        latlon = [j[0]['lat'], j[0]['lon']]
+        return city + "," + j[0]['lat'] + "," + j[0]['lon']
+    except Exception:
+        return ","
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run()
