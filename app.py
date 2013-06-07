f4e091f2b21aa577a1650e6819a22c4053c5f569
Ryder Moody <rydthemoodster@gmail.com>
2013-06-07 14:03:29 -0400

    Move api calls into json endpoint

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 from flask import Flask, render_template, request
 from flask import jsonify
 import requests
+import simplejson as json
+import datetime
 app = Flask(__name__)
 
 @app.route("/")
@@ -9,8 +11,20 @@ def home():
 
 @app.route("/city_latlon")
 def get_latlon_from_city():
-    city = request.args.get('city')
-    return city 
+    access_token = "bafda5b39a752043052cbec72ae9908e5e15c9d4"
+    endpoint = "https://api-ssl.bitly.com/v3/search"
+
+    city_list = ['us-il-chicago']
+    for city in city_list:
+        query_params = { 'access_token': access_token
+        , 'limit': 50
+        , 'city': city
+        , 'fields': 'score,title,url'
+        }
+	response = requests.get(endpoint, params= query_params)
+	data = json.loads(response.content)
+
+    return jsonify(data)
 
 if __name__ == "__main__":
     app.run(debug=True)
--- topstories.py
+++ topstories.py
@@ -9,15 +9,7 @@ endpoint = "https://api-ssl.bitly.com/v3/search"
 
 city_list = ['us-il-chicago']
 
-for city in city_list:
-	query_params = { 'access_token': access_token
-	, 'limit': 50
-	, 'city': city 
-	, 'fields': 'score,title,url'
-	}
 
-	response = requests.get(endpoint, params= query_params)
-	data = json.loads(response.content)
 
 	print data
 
