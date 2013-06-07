8e7152ee581cf09fceb3799e8c5bfbee9de7b241
harbolkn <harbolkn@gmail.com>
2013-06-07 14:42:37 -0400

    Search for top story of cities in cities.csv

--- app.py
+++ app.py
@@ -2,8 +2,7 @@ from flask import Flask, render_template, request
 from flask import jsonify
 import simplejson as json
 import datetime
-import requests
-
+import requests 
 app = Flask(__name__)
 
 @app.route("/")
@@ -15,17 +14,19 @@ def data():
     access_token = "bafda5b39a752043052cbec72ae9908e5e15c9d4"
     endpoint = "https://api-ssl.bitly.com/v3/search"
 
-    city_list = ['us-il-chicago']
-    for city in city_list:
-        query_params = { 'access_token': access_token
-        , 'limit': 50
-        , 'city': city
-        , 'fields': 'score,title,url'
-        }
-	response = requests.get(endpoint, params= query_params)
-	data = json.loads(response.content)
+	cities = [ line.split(', ')[1] for line in open('./cities.csv') ]
+	top = {}
+	for city in cities:
+		query_params = {'access_token': access_token,
+		'limit': 1,
+		'cities': city,
+		'fields': 'title,url'}
 
-    return jsonify(data)
+		response = requests.get(endpoint, params= query_params)
+		data = json.loads(response.contents)
+		top[city] = [data['url'], data['title']]
+
+	return jsonify(top)
 
 @app.route("/city_latlon")
 def get_latlon_from_city():
