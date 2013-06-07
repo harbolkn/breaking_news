b33af700cff7683dabcde730e6d3887a5fb682a9
Ryder Moody <rydthemoodster@gmail.com>
2013-06-07 13:00:34 -0400

    Add angular

--- app.py
+++ app.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template
 from flask import jsonify
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
+@app.route("/data")
+def data():
     blah = {'dave':100, 'blah':200}
     return jsonify(blah)
 
+@app.route("/")
+def home():
+    return render_template('index.html')
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
