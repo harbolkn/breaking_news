c428f3138035d1b7719e5ac366ad8d5ed8d84459
Dave Marchevsky <dm@bit.ly>
2013-06-07 12:52:54 -0400

    get city endpoint

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from flask import jsonify
+import requests
 app = Flask(__name__)
 
 @app.route("/")
@@ -7,5 +8,12 @@ def hello():
     blah = {'dave':100, 'blah':200}
     return jsonify(blah)
 
+
+@app.route("/city_latlon")
+def get_latlon_from_city():
+    city = request.args.get('city')
+    return city 
+    
+
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
